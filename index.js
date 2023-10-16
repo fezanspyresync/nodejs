c1f61e6c6ded6d170209863ddf951d81b22d5d26
Umair khan <umair.khan@spyresync.com>
2023-10-16 13:36:05 +0500

    write query

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const http = require('http');
+const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World!");
+  console.log("ready to die");
 });
 
 server.listen(PORT, () => {
